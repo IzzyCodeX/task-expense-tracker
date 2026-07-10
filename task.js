5bb726d836785313c0471e38046a5497bb979a99
IzzyCodeX <adelusiisrael87@gmail.com>
2026-07-10 20:26:51 +0100

    First commit in this project

---
+++ task.js
@@ -0,0 +1,16 @@
+const todoForm = document.querySelector(".todo-form");
+const todoList = document.querySelector(".todo-list");
+
+let tasks = [];
+todoForm.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const input = this.taskInput;
+    const inputValue = input.value;
+
+    if (inputValue !== ""){
+        const task = {
+            
+        }
+    }
+})
